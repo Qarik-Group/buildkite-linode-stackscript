85719813062ef30b459f8307e41aa139a9edcc54
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-10 09:04:53 +1000

    Ensure kubectl/k3s are useable by buildkite user

--- samples/bootstrap-scripts/install-k3s.sh
+++ samples/bootstrap-scripts/install-k3s.sh
@@ -5,6 +5,9 @@
 
 curl -sfL https://get.k3s.io | sh -
 
+# Ensure kubectl/k3s are useable by buildkite user
+mkdir -p /etc/rancher/k3s
+touch /etc/rancher/k3s/k3s.yaml
 chown -Rh buildkite:buildkite /etc/rancher/k3s
 
 k3s kubectl get nodes
