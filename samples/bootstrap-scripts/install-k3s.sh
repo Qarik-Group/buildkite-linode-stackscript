87a84f4492ecf8490779c5336116f786b752db4a
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-10 08:58:05 +1000

    make k3s accessible locally to buildkite user

--- samples/bootstrap-scripts/install-k3s.sh
+++ samples/bootstrap-scripts/install-k3s.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Install options
+# https://rancher.com/docs/k3s/latest/en/installation/install-options/
+
 curl -sfL https://get.k3s.io | sh -
+
+chown -Rh buildkite:buildkite /etc/rancher/k3s
+
+k3s kubectl get nodes
