e19a953599c29a9d93fcbecbf61c040d433c040a
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-10 10:20:23 +1000

    trying to fix: environment: k3s: command not found

--- samples/bootstrap-scripts/install-k3s.sh
+++ samples/bootstrap-scripts/install-k3s.sh
@@ -7,7 +7,11 @@ sed -i "s/tags=/ s/$/k3s-role=master/" $BUILDKITE_DIR/buildkite-agent.cfg
 # https://rancher.com/docs/k3s/latest/en/installation/install-options/
 
 curl -sfL https://get.k3s.io -o /tmp/install-k3s.sh
-# sh < /tmp/install-k3s.sh
+chmod +x /tmp/install-k3s.sh
+export INSTALL_K3S_SKIP_START=true
+/tmp/install-k3s.sh || { echo "Failed for the moment"; }
+
+service k3s start
 
 # Ensure kubectl/k3s are useable by buildkite user
 mkdir -p /etc/rancher/k3s
